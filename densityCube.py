c002c4f50c15b1b25d6979808262b5c665d95e71
Ola <jarco.aleksandra@gmail.com>
2014-09-23 22:21:38 +0200

    update

--- densityCube.py
+++ densityCube.py
@@ -14,7 +14,7 @@ Script reads Gaussian cube file (cubefile.txt) and does these things:
 import argparse
 from numpy import array
 import sys
-
+import re
 
 def argp():
     ''' parse arguments given by user in command line'''
@@ -40,41 +40,123 @@ class DensityCube(object):
         self.densityCubeFile=open(argp().cubeFile)
         self.densityCoordFile=open('density_coord.csv','w')
         self.densityIntegralsFile=open('integrals.csv','w')
-        
-    def readDensityCubeFile(self):
-        
-        self.d_range=argp().d_range
-        #print self.d_range
-        #sys.exit()
-        coordsLine= self.densityCubeFile.readlines()[2].split()
-        self.xCoord=float(coordsLine[1])
-        self.yCoord=float(coordsLine[2])
-        self.zCoord=float(coordsLine[3])
-        #print self.xCoord, self.yCoord,self.zCoord
-        self.densityCubeFile.seek(0)
+        self.densityDerivativesFile=open('derivatives','w')
+        self.grid_list=[]
+    	
 
-        self.xVoxelLine=self.densityCubeFile.readlines()[3].split()
-        self.xVoxel, self.xVoxelWidth=float(self.xVoxelLine[0]), float(self.xVoxelLine[1])
-        self.densityCubeFile.seek(0)
-        self.yVoxelLine=self.densityCubeFile.readlines()[4].split()
-        self.yVoxel, self.yVoxelWidth=float(self.yVoxelLine[0]), float(self.yVoxelLine[2])
-        self.densityCubeFile.seek(0)
-        self.zVoxelLine=self.densityCubeFile.readlines()[5].split()
-        self.zVoxel, self.zVoxelWidth=float(self.zVoxelLine[0]), float(self.zVoxelLine[3])
+    def Proccess_cube_file(self):
+    	self.title=''
+    	finishTitle=0
+    	lineNumber=0
+    	
+    	#find title
+    	while finishTitle==0:
+    	 	line=self.densityCubeFile.readline()
+    	 	if re.search('[a-zA-Z]+',line):
+    	 		self.title+=line
+    	 		lineNumber+=1
+    	 		lastPos=self.densityCubeFile.tell()
+    	 	else:
+    	 		finishTitle=1
+        self.title=self.title[0:-1]
+    	self.densityCubeFile.seek(lastPos)
+    	
+    	#find atom
+    	self.atomsLine=self.densityCubeFile.readline()
+    	self.atomsNumber,self.atom_x_origin,self.atom_y_origin,self.atom_z_origin=self.atomsLine.split()
+    	self.atomsNumber=int(self.atomsNumber)
+    	#find voxels
+    	self.xVoxelLine=self.densityCubeFile.readline().split()
+    	self.xVoxel, self.xVoxelWidth=float(self.xVoxelLine[0]), float(self.xVoxelLine[1])
+    	self.yVoxelLine=self.densityCubeFile.readline().split()
+    	self.xVoxel, self.yVoxelWidth=float(self.yVoxelLine[0]), float(self.yVoxelLine[1])
+    	self.zVoxelLine=self.densityCubeFile.readline().split()    	
+    	self.zVoxel, self.zVoxelWidth=float(self.zVoxelLine[0]), float(self.zVoxelLine[1])
+    	
+    	
+    	self.atomsDict={}
+    	for i in range(self.atomsNumber):
+    		lineNumber+=1
+    		atomLine=self.densityCubeFile.readline()
+    		atomNr,atomNr2,atom_x,atom_y,atom_z=atomLine.split()
+    		self.atomsDict[i]=[atomNr,atom_x,atom_y,atom_z]
+    	
+    	 		
+    	#print self.densityCubeFile.readline()
+    	for line in self.densityCubeFile.readlines():
+             for grid in line.split():
+                 self.grid_list.append(grid)
+     #=======================================================================
+    	#print self.grid_list[-1]
+    	#print atomsDict
+    		
+    	#self.atomsInfo=
+    	#print atomsNumber
+     	#print self.atomsLine
+    
+    def d2f_dz2(self,z,y,x): #1
+        if z<self.xVoxel-1 and z>0:
+            return (((a[z+1][y][x])-2*float(a[z][y][x])+(a[z-1][y][x]))/(self.delta_z*self.delta_z))
+        else:
+            return None 
+ 
+    def d2f_dy2(self,z,y,x,delta_y):#2
+        if y<self.zVoxel-1 and y>0:        
+            return  (((a[z][y+1][x])-2*float(a[z][y][x])+(a[z][y-1][x]))/(self.delta_y*self.delta_y))
+        else:
+            return None
+ 
+    def d2f_dx2(self,z,y,x): #3
+        if x<self.zVoxel-1 and x>0:
+            return ((a[z][y][x+1]-2*float(a[z][y][x])+(a[z][y][x-1]))/(self.delta_x*self.delta_x))
+        else:
+            return None
+     
+     
+    def d2f_dzdy(self,z,y,x): #4
+        if y<self.zVoxel-1 and y>0 and z<self.xVoxel-1 and z>0:
+            return ((a[z+1][y+1][x]-float(a[z+1][y-1][x])-float(a[z-1][y+1][x])+float(a[z-1][y-1][x]))/(4*self.delta_z*self.delta_y))
+        else:
+            return None
+
+    
+    def d2f_dzdx(self,z,y,x): #5   
+        if x<self.yVoxel-1 and x>0 and z<self.xVoxel-1 and z>0:
+            return (((a[z+1][y][x+1]-float(a[z+1][y][x-1])-float(a[z-1][y][x+1])+float(a[z-1][y][x+1]))/(4*self.delta_z*self.delta_y)))
+        else:
+            return None
+
+  
+    def d2f_dydx(self,z,y,x): #6    
+        if y<self.zVoxel-1 and y>0 and x<self.zVoxel-1 and x>0:
+            return (((a[z][y+1][x+1]-float(a[z][y+1][x-1])-float(a[z][y-1][x+1])+float(a[z][y-1][x+1]))/(4*self.delta_y*self.delta_x)))
+        else:
+            return None
+    
+    
+    def d2f_dydz(self,z,y,x): #7
+        if y<self.zVoxel-1 and y>0 and z<self.xVoxel-1 and z>0:
+            return  (((a[z+1][y+1][x]-float(a[z-1][y+1][x])-float(a[z+1][y-1][x])+float(a[z+1][y-1][x]))/(4*self.delta_y*self.delta_z)))
+        else:
+            return None
+    
+    
+    def d2f_dxdz(self,z,y,x): #8
+        if x<self.yVxel-1 and x>0 and z<self.xVoxel-1 and z>0:
+            return (((a[z+1][y][x+1]-float(a[z-1][y][x+1])-float(a[z+1][y][x-1])+float(a[z+1][y][x-1]))/(4*self.delta_x*self.delta_z)))
+        else:
+            return None
+    
+    def d2f_dxdy(self,z,y,x): #9
+        if x<self.yVoxel-1 and x>0 and y<self.zVoxel-1 and y>0:
+            return ((a[z][y+1][x+1]-float(a[z][y-1][x+1])-float(a[z][y-1][x-1])+float(a[z][y-1][x-1]))/(4*self.delta_x*self.delta_y))
+        else:
+            return None    
         
-        self.densityCubeFile.seek(0)
-        self.grid_list=[] 
         
-        for line in self.densityCubeFile.readlines()[10:]:
-            for grid in line.split():
-                self.grid_list.append(grid)
-        
-        #self.grid_list=[grid for grid in  [line.split() for line  in self.densityCubeFile.readlines()[10:]]]
-        #print self.grid_list[0],self.grid_list[-1]
-        print self.xVoxel, self.yVoxel, self.zVoxel
-        print self.xVoxelWidth, self.yVoxelWidth, self.zVoxelWidth
-        
-        
+    def hessian(self,z,y,x):
+       	return self.d2f_dx2(z,y,x),self.d2f_dxdy(z,y,x),self.d2f_dzdx(z,y,x),self.d2f_dxdy(z,y,x),self.d2f_dy2(z,y,x),self.d2f_dzdy(z,y,x),self.d2f_dxdz(z,y,x),self.d2f_dydz(z,y,x),self.d2f_dz2(z,y,x)
+    
     def createCoordsDensityFile(self):
         
         self.coord_list=[] #lista wpolrzednych wszystkich gridow
@@ -129,11 +211,23 @@ class DensityCube(object):
                                 print >> self.densityIntegralsFile, integral
         
         
-
+    def Calculate_hessian_for_each_point(self):
+    	for z in range(0,len(a)):
+        	for y in range(0,len(a[z])):
+          		for x in range(0,len(a[z][y])):
+            		 print>>self.densityDerivativesFile,self.hessian(z,y,x)
+        
+        
 
 if __name__ == '__main__':
-    DC=DensityCube()
-    DC.readDensityCubeFile()
-    #DC.createCoordsDensityFile()
-    DC.IntegrateDensity()
+	
+	DC=DensityCube()
+	DC.Proccess_cube_file()
+	print DC.title
+	sys.exit()
+   	DC.readDensityCubeFile()
+    #sys.exit()
+	DC.createCoordsDensityFile()
+	DC.Calculate_hessian_for_each_point()
+	DC.IntegrateDensity()
                 
